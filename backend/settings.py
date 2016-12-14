a7fb7f0efa27ec0a11ebc9e11df4140bc4bad0c8
Nitish Bhat <nbhat@gatech.edu>
2016-12-14 12:19:49 -0500

    Change DB
    Signed-off-by: Nitish Bhat <nbhat@gatech.edu>

--- backend/settings.py
+++ backend/settings.py
@@ -79,13 +79,13 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'sql_server.pyodbc',
-        'NAME': 'nbhatdb',
+        'NAME': 'bookquery',
 	'USER': 'microsoft@bookquery',
 	'PASSWORD': '!Bhatnitish1',
 	'HOST': 'bookquery.database.windows.net',
 	'PORT': '1433',
 	'OPTIONS': {
-		'driver': 'SQL Server Native Client 11.0',
+		'driver': 'ODBC Driver 13 for SQL Server',
 		'MARS_Connection': 'True',
 	}
     }
